bdcfc2eb8fbbf3a84515485856cb0ba3f05d1310
ArchLeaders <archleadership28@gmail.com>
2023-10-31 03:13:30 -0700

    Add TruncatedString IValueConverter

---
+++ src/DotnetLive/Converters/TruncatedStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+
+namespace DotnetLive.Converters;
+
+public class TruncatedStringConverter : IValueConverter
+{
+    private const int OverflowLength = 115;
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string str && str.Length > OverflowLength) {
+            return str[..(OverflowLength - 5)] + ". . .";
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
